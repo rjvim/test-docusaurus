eec772822a3eac47d314b2e22c70532b838ef062
Rajiv Seelam <rajiv@betalectic.com>
2024-10-10 19:49:40 +0530

    Change title again

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -3,7 +3,7 @@ import type { Config } from "@docusaurus/types";
 import type * as Preset from "@docusaurus/preset-classic";
 
 const config: Config = {
-  title: "My Site Main",
+  title: "My Site Main 2",
   tagline: "Dinosaurs are cool",
   favicon: "img/favicon.ico",
 
